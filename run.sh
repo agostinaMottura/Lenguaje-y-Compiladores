690f2b086988803de085d97d0bdf6a3f5b4d3e79
Facundo Carballo <59286028+facundocarballo@users.noreply.github.com>
2025-08-29 11:58:36 -0300

    Script - Run all tests (#2)
    * feat: script-run-all-tests
    
    * fix: script

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 flex Lexico.l
 bison -dyv Sintactico.y
 gcc lex.yy.c y.tab.c -o compilador
-./compilador prueba.txt
+if [ -z "$1" ]; then
+    ./compilador prueba.txt
+else
+    ./compilador tests/$1
+fi
 rm lex.yy.c
 rm y.tab.c
 rm y.output
