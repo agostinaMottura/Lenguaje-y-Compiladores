dd695549c5389de373358cd3207adac75e0f7175
facundocarballo <carballofacundo70@gmail.com>
2025-10-05 13:20:40 -0300

    refactor: validaciones lexico

--- src/lexico/validaciones/validaciones.c
+++ src/lexico/validaciones/validaciones.c
@@ -4,9 +4,9 @@
 #include <math.h>
 #include "./validaciones.h"
 
-ResultadoValidacion es_string_valido(char cadena[])
+t_validaciones_lexico_resultado_validacion validaciones_lexico_es_string_valido(char cadena[])
 {
-    ResultadoValidacion resultado;
+    t_validaciones_lexico_resultado_validacion resultado;
     int largo = strlen(cadena);
 
     if (largo > MAX_STRING_LONGITUD)
@@ -21,9 +21,9 @@ ResultadoValidacion es_string_valido(char cadena[])
     return resultado;
 }
 
-ResultadoValidacion es_float_valido(char *cadena)
+t_validaciones_lexico_resultado_validacion validaciones_lexico_es_float_valido(char *cadena)
 {
-    ResultadoValidacion resultado;
+    t_validaciones_lexico_resultado_validacion resultado;
     char *endptr;
     double numero = strtod(cadena, &endptr); // strtod convierte la cadena a double y deja en endptr un puntero al primer carácter que no pudo convertir.
 
@@ -37,11 +37,11 @@ ResultadoValidacion es_float_valido(char *cadena)
 
     float numero_float = (float)numero;
 
-    if (numero_float < MIN_FLOAT_VALOR || numero_float > MAX_FLOAT_VALOR)
+    if (numero_float < VALIDACIONES_LEXICO_MIN_VALOR_FLOAT || numero_float > VALIDACIONES_LEXICO_MAX_VALOR_FLOAT)
     {
         resultado.es_valido = 0;
         sprintf(resultado.mensaje_error, "Float fuera de rango");
-        sprintf(resultado.aclaracion, "Floats validos entre %.5e y %.5e", MIN_FLOAT_VALOR, MAX_FLOAT_VALOR);
+        sprintf(resultado.aclaracion, "Floats validos entre %.5e y %.5e", VALIDACIONES_LEXICO_MIN_VALOR_FLOAT, VALIDACIONES_LEXICO_MAX_VALOR_FLOAT);
         return resultado;
     }
 
@@ -57,24 +57,24 @@ ResultadoValidacion es_float_valido(char *cadena)
     return resultado;
 }
 
-ResultadoValidacion es_int_valido(char *cadena)
+t_validaciones_lexico_resultado_validacion validaciones_lexico_es_int_valido(char *cadena)
 {
-    ResultadoValidacion resultado;
+    t_validaciones_lexico_resultado_validacion resultado;
     int longitud = strlen(cadena);
-    if (longitud > MAX_INT_LONGITUD)
+    if (longitud > VALIDACIONES_LEXICO_MAX_LONGITUD_INT)
     {
         resultado.es_valido = 0;
         sprintf(resultado.mensaje_error, "Entero supera la longitud maxima permitida");
-        sprintf(resultado.aclaracion, "Maximo %d caracteres", MAX_INT_LONGITUD);
+        sprintf(resultado.aclaracion, "Maximo %d caracteres", VALIDACIONES_LEXICO_MAX_LONGITUD_INT);
         return resultado;
     }
 
     long numero_long = atol(cadena);
-    if (numero_long > MAX_INT_VALOR)
+    if (numero_long > VALIDACIONES_LEXICO_MAX_VALOR_INT)
     {
         resultado.es_valido = 0;
         sprintf(resultado.mensaje_error, "Entero fuera de rango");
-        sprintf(resultado.aclaracion, "Enteros validos hasta %d", MAX_INT_VALOR);
+        sprintf(resultado.aclaracion, "Enteros validos hasta %d", VALIDACIONES_LEXICO_MAX_VALOR_INT);
         return resultado;
     }
 
--- src/lexico/validaciones/validaciones.h
+++ src/lexico/validaciones/validaciones.h
@@ -1,25 +1,23 @@
 #ifndef VALIDACIONES_LEXICO_H
 #define VALIDACIONES_LEXICO_H
 
-#define MAX_MSG_ERROR_LONGITUD 100
+#define VALIDACIONES_LEXICO_MAX_MENSAJE_ERROR_LONGITUD 100
 
-#define MAX_STRING_LONGITUD 50
+#define VALIDACIONES_LEXICO_MAX_VALOR_INT 65535
+#define VALIDACIONES_LEXICO_MAX_LONGITUD_INT 5
 
-#define MAX_INT_VALOR 65535
-#define MAX_INT_LONGITUD 5
-
-#define MAX_FLOAT_VALOR 3.40282e+38f
-#define MIN_FLOAT_VALOR 1.17549e-38f
+#define VALIDACIONES_LEXICO_MAX_VALOR_FLOAT 3.40282e+38f
+#define VALIDACIONES_LEXICO_MIN_VALOR_FLOAT 1.17549e-38f
 
 typedef struct
 {
     int es_valido;
-    char mensaje_error[MAX_MSG_ERROR_LONGITUD];
-    char aclaracion[MAX_MSG_ERROR_LONGITUD];
-} ResultadoValidacion;
+    char mensaje_error[VALIDACIONES_LEXICO_MAX_MENSAJE_ERROR_LONGITUD];
+    char aclaracion[VALIDACIONES_LEXICO_MAX_MENSAJE_ERROR_LONGITUD];
+} t_validaciones_lexico_resultado_validacion;
 
-ResultadoValidacion es_string_valido(char cadena[]);
-ResultadoValidacion es_float_valido(char *cadena);
-ResultadoValidacion es_int_valido(char *cadena);
+t_validaciones_lexico_resultado_validacion validaciones_lexico_es_string_valido(char cadena[]);
+t_validaciones_lexico_resultado_validacion validaciones_lexico_es_float_valido(char *cadena);
+t_validaciones_lexico_resultado_validacion validaciones_lexico_es_int_valido(char *cadena);
 
 #endif // VALIDACIONES_LEXICO_H
