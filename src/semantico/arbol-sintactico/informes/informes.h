1d35d33a3347a34a8762735e81b694877ba47f1a
facundocarballo <carballofacundo70@gmail.com>
2025-10-06 19:36:55 -0300

    refactor: informes-h

--- src/semantico/arbol-sintactico/informes/informes.h
+++ src/semantico/arbol-sintactico/informes/informes.h
@@ -3,4 +3,4 @@
 
 void informes_arbol_sintactico_imprimir_mensaje(const char *mensaje);
 void informes_arbol_sintactico_imprimir_error(const char *mensaje);
-#endif // ARBOL_SINTACTICO_INFORMES_H
+#endif // INFORMES_ARBOL_SINTACTICO_H
