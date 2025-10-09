06f448f288e9314a8da838fb79160b5b0b685adf
facundocarballo <carballofacundo70@gmail.com>
2025-10-09 16:17:35 -0300

    feat: pila

---
+++ src/pila/pila.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <string.h>
+#include "pila.h"
+
+t_pila *pila_crear()
+{
+    t_pila *pila = (t_pila *)malloc(sizeof(t_pila));
+    if (pila == NULL)
+    {
+        // Informar falta memoria
+        exit(1);
+    }
+
+    pila->tope = NULL;
+
+    return pila;
+}
+
+void pila_apilar(t_pila *pila, void *dato, size_t tamano)
+{
+    t_pila_nodo *nodo = (t_pila_nodo *)malloc(sizeof(t_pila_nodo));
+    if (nodo == NULL)
+    {
+        // Informar falta memoria
+        exit(1);
+    }
+
+    nodo->dato = malloc(tamano);
+    if (nodo->dato == NULL)
+    {
+        // Informar falta memoria
+        exit(1);
+    }
+
+    memcpy(nodo->dato, dato, tamano);
+
+    nodo->sig = pila->tope;
+    pila->tope = nodo;
+}
+
+void *pila_desapilar(t_pila *pila)
+{
+    if (pila_esta_vacia(pila))
+        return NULL;
+
+    t_pila_nodo *nodo = pila->tope;
+    void *dato = nodo->dato;
+    pila->tope = nodo->sig;
+
+    free(nodo);
+
+    return dato;
+}
+
+int pila_esta_vacia(t_pila *pila)
+{
+    return pila->tope == NULL;
+}
+
+void pila_destruir(t_pila *pila)
+{
+    while (!pila_esta_vacia(pila))
+    {
+        void *dato = pila_desapilar(pila);
+        free(dato);
+    }
+
+    free(pila);
+}
