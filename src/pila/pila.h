58e045e3719c15d06a524d0442a548684c94da72
Facundo Carballo <carballofacundo70@gmail.com>
2025-10-09 16:18:23 -0300

    Pila (#23)

---
+++ src/pila/pila.h
@@ -0,0 +1,24 @@
+#ifndef PILA_H
+#define PILA_H
+
+#include <stddef.h>
+
+typedef struct nodo
+{
+    void *dato;
+    struct nodo *sig;
+} t_pila_nodo;
+
+typedef struct pila
+{
+    nodo *tope;
+} t_pila;
+
+t_pila *pila_crear();
+void pila_apilar(t_pila *pila, void *dato, size_t tamano);
+void *pila_desapilar(t_pila *pila);
+int pila_esta_vacia(t_pila *pila);
+void pila_destruir(t_pila *pila);
+// int pila_ver_tope(t_pila *pila);
+
+#endif // PILA_H
