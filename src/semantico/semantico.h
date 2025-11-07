c22d786052986aa734413ce2c3a6c3072ed6c29d
facundocarballo <carballofacundo70@gmail.com>
2025-11-07 15:55:36 -0300

    refactor: white-space

--- src/semantico/semantico.h
+++ src/semantico/semantico.h
@@ -13,8 +13,9 @@ int semantico_validacion_tipo_dato(
 int semantico_son_tipos_de_datos_compatibles(
     t_tipo_dato tipo_dato_a,
     t_tipo_dato tipo_dato_b);
+
 t_tipo_dato semantico_obtener_tipo_de_dato_resultante(
     t_tipo_dato tipo_dato_a,
     t_tipo_dato tipo_dato_b);
-
+    
 #endif // SEMANTICO_H
